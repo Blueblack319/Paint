ad61e9c0bb845c9d088881f0061b05ebb6a51891
Jae Hoon Yang <didwogns789@naver.com>
2020-08-29 09:42:04 +0900

    Add Events

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,2 +1,22 @@
 const canvas = document.getElementById("js-canvas");
 const ctx = canvas.getContext("2d");
+
+let painting = false;
+
+const handleMouseMove = (event) => {
+  if (painting === true) {
+    console.log(event);
+  }
+};
+
+if (canvas) {
+  canvas.addEventListener("mousedown", function () {
+    painting = true;
+    console.log(painting);
+  });
+  canvas.addEventListener("mouseup", function () {
+    painting = false;
+    console.log(painting);
+  });
+  canvas.addEventListener("mousemove", handleMouseMove);
+}
